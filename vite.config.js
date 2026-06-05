a334a1dc59dbf3a26601134a7ce340f5e7749fbf
Joseph Ruiz <joseph@cardinalcomputersystems.com>
2026-06-05 00:16:49 -0400

    babel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   // This is CRITICAL for GitHub Pages
   base: '/itech-development-charities/',
   build: {
     outDir: 'dist',
-    // Ensures assets are handled correctly
     rollupOptions: {
       input: './index.html'
     }
