76daf8a407dd2371b9ad40e876962551d454ea7b
Joseph Ruiz <joseph@cardinalcomputersystems.com>
2026-06-04 23:14:47 -0400

    fixing categorizaiton ai

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        itech: {
+          dark: '#051614',
+          green: '#0f3d23',
+          yellow: '#eab308'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
